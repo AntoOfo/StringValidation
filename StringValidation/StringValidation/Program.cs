481afd06a186cd2369e1728757d0f6e045fe6a82
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2023-12-15 13:57:00 +0000

    Method skeletons created

--- StringValidation/StringValidation/Program.cs
+++ StringValidation/StringValidation/Program.cs
@@ -1,11 +1,44 @@
-﻿namespace StringValidation
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace StringValidation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a sentence: ");
-            string userString = Console.ReadLine();
+
+            string userString = Console.ReadLine();     // User Input
+
+
+            static bool CheckUppercase(string upper)
+            {
+                if (char.IsUpper(upper[0]) == false)        // if first character in string is uppercase
+                {
+                    Console.WriteLine("Sentence needs to start with capital letter.")
+                    return false;
+                }
+
+                return true;
+
+            }
+
+            static bool EvenQuotation(string quotation)
+            {
+                int count = 0;
+
+
+            }
+
+            static bool EndOfSentence(string sentence)
+            {
+
+            }
+
+            static bool NumberSpellOut(string sentence)
+            {
+
+            }
 
 
 
